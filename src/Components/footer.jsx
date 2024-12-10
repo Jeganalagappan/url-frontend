bea9d455e8a7ec81c31b6c688cfa3f1802c9393d
Jeganalagappan <149089899+Jeganalagappan@users.noreply.github.com>
2024-12-10 21:55:37 +0530

    commit

---
+++ src/Components/footer.jsx
@@ -0,0 +1,22 @@
+
+const Footer = () =>{
+    return(
+        <>
+        {/* <!-- Footer -->*/}
+<footer className="text-center text-lg-start bg-light text-muted" style={{marginTop:"40vh"}}>
+  
+
+  {/* <!-- Copyright -->*/}
+  <div className="text-center p-4" style={{backgroundColor: "rgba(0, 8, 6, 0.05)"}}>
+    © 2024 Copyright : 
+    <br/>
+    <a className="text-reset fw-bold" href=" http://localhost:5173/">Jega(URL Shortner)</a>
+  </div>
+  {/* <!-- Copyright -->*/}
+</footer>
+{/* <!-- Footer -->*/}
+        </>
+    )
+}
+
+export default Footer
